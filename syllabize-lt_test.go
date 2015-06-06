f43d14a5e24f2ad8ae6dbeaa019015e2ae8deee9
Vytautas Šaltenis <vytas@rtfb.lt>
2015-06-06 12:01:49 +0300

    Reduce duplication in tests

--- syllabize-lt_test.go
+++ syllabize-lt_test.go
@@ -5,11 +5,24 @@ import (
 	"testing"
 )
 
+type StrToStrListTest struct {
+	word string
+	exp  []string
+}
+
+func LoopTests(t *testing.T, tests []StrToStrListTest,
+	testedFunc func(w string) []string, name string) {
+	for _, test := range tests {
+		actual := testedFunc(test.word)
+		if !reflect.DeepEqual(actual, test.exp) {
+			t.Errorf("%s(%q) = %v, but expected %v\n", name, test.word, actual,
+				test.exp)
+		}
+	}
+}
+
 func TestSplitSounds(t *testing.T) {
-	var tests = []struct {
-		word string
-		exp  []string
-	}{
+	var tests = []StrToStrListTest{
 		{"labas", []string{"l", "a", "b", "a", "s"}},
 		{"rytas", []string{"r", "y", "t", "a", "s"}},
 		{"malonu", []string{"m", "a", "l", "o", "n", "u"}},
@@ -18,20 +31,11 @@ func TestSplitSounds(t *testing.T) {
 		{"džipas", []string{"dž", "i", "p", "a", "s"}},
 		{"dzūkas", []string{"dz", "ū", "k", "a", "s"}},
 	}
-	for _, test := range tests {
-		actual := splitSounds(test.word)
-		if !reflect.DeepEqual(actual, test.exp) {
-			t.Errorf("splitSounds(%q) = %v, but expected %v\n", test.word,
-				actual, test.exp)
-		}
-	}
+	LoopTests(t, tests, splitSounds, "splitSounds")
 }
 
 func TestSyllabificate(t *testing.T) {
-	var tests = []struct {
-		word string
-		exp  []string
-	}{
+	var tests = []StrToStrListTest{
 		{"labas", []string{"la", "bas"}},
 		{"rytas", []string{"ry", "tas"}},
 		{"malonu", []string{"ma", "lo", "nu"}},
@@ -40,11 +44,5 @@ func TestSyllabificate(t *testing.T) {
 		{"džipas", []string{"dži", "pas"}},
 		{"dzūkas", []string{"dzū", "kas"}},
 	}
-	for _, test := range tests {
-		actual := syllabificate(test.word)
-		if !reflect.DeepEqual(actual, test.exp) {
-			t.Errorf("syllabificate(%q) = %v, but expected %v\n", test.word,
-				actual, test.exp)
-		}
-	}
+	LoopTests(t, tests, syllabificate, "syllabificate")
 }
