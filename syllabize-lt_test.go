c7de907dc4456968f8ce36309c3fc30dfc94c764
Vytautas Šaltenis <vytas@rtfb.lt>
2015-06-06 12:06:37 +0300

    Make tests more readable

--- syllabize-lt_test.go
+++ syllabize-lt_test.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
 type StrToStrListTest struct {
 	word string
-	exp  []string
+	exp  string
 }
 
 func LoopTests(t *testing.T, tests []StrToStrListTest,
 	testedFunc func(w string) []string, name string) {
 	for _, test := range tests {
-		actual := testedFunc(test.word)
+		actual := strings.Join(testedFunc(test.word), "-")
 		if !reflect.DeepEqual(actual, test.exp) {
 			t.Errorf("%s(%q) = %v, but expected %v\n", name, test.word, actual,
 				test.exp)
@@ -23,26 +24,26 @@ func LoopTests(t *testing.T, tests []StrToStrListTest,
 
 func TestSplitSounds(t *testing.T) {
 	var tests = []StrToStrListTest{
-		{"labas", []string{"l", "a", "b", "a", "s"}},
-		{"rytas", []string{"r", "y", "t", "a", "s"}},
-		{"malonu", []string{"m", "a", "l", "o", "n", "u"}},
-		{"jus", []string{"j", "u", "s"}},
-		{"matyti", []string{"m", "a", "t", "y", "t", "i"}},
-		{"džipas", []string{"dž", "i", "p", "a", "s"}},
-		{"dzūkas", []string{"dz", "ū", "k", "a", "s"}},
+		{"labas", "l-a-b-a-s"},
+		{"rytas", "r-y-t-a-s"},
+		{"malonu", "m-a-l-o-n-u"},
+		{"jus", "j-u-s"},
+		{"matyti", "m-a-t-y-t-i"},
+		{"džipas", "dž-i-p-a-s"},
+		{"dzūkas", "dz-ū-k-a-s"},
 	}
 	LoopTests(t, tests, splitSounds, "splitSounds")
 }
 
 func TestSyllabificate(t *testing.T) {
 	var tests = []StrToStrListTest{
-		{"labas", []string{"la", "bas"}},
-		{"rytas", []string{"ry", "tas"}},
-		{"malonu", []string{"ma", "lo", "nu"}},
-		{"jus", []string{"jus"}},
-		{"matyti", []string{"ma", "ty", "ti"}},
-		{"džipas", []string{"dži", "pas"}},
-		{"dzūkas", []string{"dzū", "kas"}},
+		{"labas", "la-bas"},
+		{"rytas", "ry-tas"},
+		{"malonu", "ma-lo-nu"},
+		{"jus", "jus"},
+		{"matyti", "ma-ty-ti"},
+		{"džipas", "dži-pas"},
+		{"dzūkas", "dzū-kas"},
 	}
 	LoopTests(t, tests, syllabificate, "syllabificate")
 }
